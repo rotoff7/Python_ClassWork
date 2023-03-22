297c1e3c8d47c2728534d32cba92bd6c1afd98b9
Maxim Gostry <rotoff7@mail.ru>
2023-03-23 00:13:41 +0300

    BaseTask#3

--- HomeWork#3/Base_Task3_3.py
+++ HomeWork#3/Base_Task3_3.py
@@ -21,10 +21,78 @@
 # “ед”: [“шт.”]
 # }
 
-def ValueCheck():
+
+# i = 1
+# goods = list()
+# goods.append((i, {input('Название характеристики: '): input('Значение: ')}))
+
+class QuestionCheck(Exception):
+    pass
+
+
+def value_check():
     try:
         val = int(input())
     except ValueError:
         print('Введено некорректное значение.')
         exit(0)
+    else:
+        return val
 
+
+# Не придумал как в такой функции обрабатывать исключения, чтоб сохранить пояснение.
+def add_product(goods_array, index):
+    goods_array.append((index, {'Название': input('Введите название товара:'),
+                                'Цена': int(input('Введите стоимость товара: ')),
+                                'Кол-во': int(input('Введите кол-во товара: ')),
+                                'Ед': input('Введите единицу измерения товара (шт./кг/л): ')}))
+
+
+def goods_printing(goods_array):
+    print('Текущий список товаров: ')
+    for el in goods_array:
+        print(el)
+
+
+goods = list()
+
+print('Хотите добавить товар? (1 - да, 0 - нет):')
+try:
+    question = value_check()
+    if question != 0 and question != 1:
+        raise QuestionCheck()
+except QuestionCheck:
+    print('Введено некорректное значение. Допустимые значения: 1/0')
+    exit(0)
+
+if question == 1:
+    flag = True
+    i = 1
+    while flag:
+        add_product(goods, i)
+        print(f'Товар с идентификатором "{i}" успешно добавлен.')
+        i += 1
+        tmp = int(input('Хотите продолжить добавлять товары? (1 - да, 0 - нет):'))
+        if tmp == 0:
+            flag = False
+    goods_printing(goods)
+
+    goods_names = []
+    goods_prices = []
+    goods_counts = []
+    goods_unit_type = []
+    goods_analys = {}
+
+    for i in range(len(goods)):
+        goods_names.append(goods[i][1]['Название'])
+        goods_prices.append(goods[i][1]['Цена'])
+        goods_counts.append(goods[i][1]['Кол-во'])
+        goods_unit_type.append(goods[i][1]['Ед'])
+
+    goods_analys.update({'Названия': goods_names, 'Цены': goods_prices, 'Кол-ва': goods_counts, 'Ед': goods_unit_type})
+    print('Аналитика товарной информации:')
+    for k, v in goods_analys.items():
+        print(f'{k}: ', v)
+
+else:
+    print('Сохраненные товары отсутствуют.')
