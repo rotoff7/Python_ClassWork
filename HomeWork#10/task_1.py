0077ce89e811883506281a3a08be91ee78bfc9e6
Maxim Gostry <rotoff7@mail.ru>
2023-04-13 17:13:54 +0300

    task_1

--- HomeWork#10/task_1.py
+++ HomeWork#10/task_1.py
@@ -15,3 +15,32 @@
 --- '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430' - набор кодовых точек
 --- используйте списки и циклы, не дублируйте функции
 """
+
+
+# 1 версия. Тип данных: байты (b'\\u0440....)
+# def code_points(array):
+#     c_p_array = []
+#     for el in array:
+#         c_p_array.append(el.encode("unicode_escape", "utf-8"))
+#     return c_p_array
+
+def code_points(array):
+    c_p_array = []
+    for el in array:
+        temp = (el.encode("unicode_escape", "utf-8"))
+        c_p_array.append(temp.decode('utf-8'))
+    return c_p_array
+
+
+def result_printing(w_array, cp_array):
+    for i in range(len(w_array)):
+        print(f'Слово "{w_array[i]}" - буквенный формат.')
+        print(f'Тип данных: {type(w_array[i])}')
+        print(f'{cp_array[i]} - набор кодовых точек')
+        print(f'Тип данных: {type(cp_array[i])}')
+        print('')
+
+
+words = ['разработка', 'сокет', 'декоратор']
+code_points_arr = code_points(words)
+result_printing(words, code_points_arr)
