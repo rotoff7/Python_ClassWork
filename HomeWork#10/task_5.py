2afcdd7d1a94b20bd830d35566c5fbeccc58b2a6
Maxim Gostry <rotoff7@mail.ru>
2023-04-13 21:43:35 +0300

    task5 + HomeWork10_Complete

--- HomeWork#10/task_5.py
+++ HomeWork#10/task_5.py
@@ -7,3 +7,13 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+
+import subprocess
+import chardet
+
+args = ['ping', 'yandex.ru']
+ya_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+for el in ya_ping.stdout:
+    result = chardet.detect(el)
+    el = el.decode(result['encoding']).encode('utf-8')
+    print(el.decode('utf-8'))
