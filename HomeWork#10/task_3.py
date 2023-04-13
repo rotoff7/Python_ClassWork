2cce60e9335d4b628e8711ef41a783dab1862cc1
Maxim Gostry <rotoff7@mail.ru>
2023-04-13 19:49:17 +0300

    task3

--- HomeWork#10/task_3.py
+++ HomeWork#10/task_3.py
@@ -9,3 +9,18 @@
 --- обязательно!!! усложните задачу, "отловив" исключение,
 придумайте как это сделать
 """
+
+
+def b_words(array):
+    res_arr = []
+    try:
+        for el in array:
+            res_arr.append(bytes(el, 'ascii'))
+    except UnicodeEncodeError:
+        print("Слово невозможно записать в байтовом типе с помощью маркировки b''")
+    return res_arr
+
+
+words = ['attribute', 'класс', 'функция', 'type']
+result = b_words(words)
+print(result)
