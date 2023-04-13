2cf0226105341bdcb2b27464f4d83d7090ef088e
Maxim Gostry <rotoff7@mail.ru>
2023-04-13 19:31:51 +0300

    task4

--- HomeWork#10/task_4.py
+++ HomeWork#10/task_4.py
@@ -8,3 +8,28 @@
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
 """
+
+
+def bytes_convert(array):
+    res_arr = []
+    for el in array:
+        res_arr.append(el.encode('utf-8'))
+    return res_arr
+
+
+def words_convert(array):
+    res_arr = []
+    for el in array:
+        res_arr.append(el.decode('utf-8'))
+    return res_arr
+
+
+words = ['разработка', 'администрирование', 'protocol', 'standard']
+print('Байтовое представление:')
+b_words = bytes_convert(words)
+for element in b_words:
+    print(element)
+print('')
+print('Буквенное представление:')
+let_words = words_convert(b_words)
+print(let_words)
