e20be355e9564f45192401ac3260013cba736a3c
Maxim Gostry <rotoff7@mail.ru>
2023-04-13 17:32:10 +0300

    task2

--- HomeWork#10/task_2.py
+++ HomeWork#10/task_2.py
@@ -10,3 +10,21 @@
 --- b'class' - используйте маркировку b''
 --- используйте списки и циклы, не дублируйте функции
 """
+
+
+# Хотел сделать просто через приписку b'', но не понимаю
+# как внутрь поместить содержимое елемента массива, а не слово 'el'
+def b_words(array):
+    res_arr = []
+    for el in array:
+        res_arr.append(bytes(el, 'utf-8'))
+    return res_arr
+
+
+def result_print(array):
+    for el in array:
+        print(f'Содержимое: {el}. Тип: {type(el)}. Длина: {len(el)}')
+
+
+words = ['class', 'function', 'method']
+result_print(b_words(words))
